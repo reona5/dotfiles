a4b07cae55b918ee63b63a148793b5dc2f76be02
mpg-reona-shimada <reona.shimada@medpeer.co.jp>
2022-12-26 22:17:58 +0900

    Invalidate default plugins for neovim.

--- .config/nvim/lua/base/option.lua
+++ .config/nvim/lua/base/option.lua
@@ -15,6 +15,24 @@ vim.o.whichwrap = "b,s,h,l,[,],<,>"
 vim.o.signcolumn = "yes"
 
 vim.cmd([[
+  let g:did_install_default_menus = 1
+  let g:did_install_syntax_menu   = 1
+  let g:did_indent_on             = 1
+  let g:did_load_ftplugin         = 1
+  let g:loaded_2html_plugin       = 1
+  let g:loaded_gzip               = 1
+  let g:loaded_man                = 1
+  let g:loaded_matchit            = 1
+  let g:loaded_matchparen         = 1
+  let g:loaded_netrwPlugin        = 1
+  let g:loaded_remote_plugins     = 1
+  let g:loaded_shada_plugin       = 1
+  let g:loaded_spellfile_plugin   = 1
+  let g:loaded_tarPlugin          = 1
+  let g:loaded_tutor_mode_plugin  = 1
+  let g:loaded_zipPlugin          = 1
+  let g:skip_loading_mswin        = 1
+
 	" Full-width Space highlight
 	function! Space()
 		highlight Space cterm=underline ctermfg=lightblue guibg=darkgray

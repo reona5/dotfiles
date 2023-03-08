a06e71f0f32bf4396eb68ce95f2e29b6a6a7a84e
mpg-reona-shimada <reona.shimada@medpeer.co.jp>
2023-03-08 13:59:34 +0900

    Update

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -7,13 +7,6 @@ vim.cmd([[
 			endif
 	endfunction
 	command ToggleGStatus :call ToggleGStatus()
-
-	" Status line
-	if !exists('*fugitive#statusline')
-		set statusline=%F\ %m%r%h%w%y%{'['.(&fenc!=''?&fenc:&enc).':'.&ff.']'}[L%l/%L,C%03v]
-		set statusline+=%=
-		set statusline+=%{fugitive#statusline()}
-	endif
 ]])
 
 local opts = {noremap = true, silent = true}
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,27 +4,26 @@ if (not status) then return end
 lualine.setup {
   options = {
     icons_enabled = true,
-    section_separators = {'', ''},
-    component_separators = {'', ''},
+    section_separators = { '', '' },
+    component_separators = { '', '' },
     disabled_filetypes = {},
     globalstatus = true
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
   tabline = {},
-  extensions = {'fugitive'}
 }

a699be4e32bc79c2d90f13477924c99d3c5edb8b
mpg-reona-shimada <reona.shimada@medpeer.co.jp>
2024-08-03 15:08:46 +0900

    Update

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -1,13 +1,16 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -174,6 +177,18 @@ require('lazy').setup({
   },
   {
     "github/copilot.vim",
-    lazy = false,
+    event = { 'InsertEnter' },
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    event = { 'BufRead' },
+    branch = "canary",
+    dependencies = {
+      { "github/copilot.vim", lazy = true },
+      { "nvim-lua/plenary.nvim", lazy = true },
+    },
+    opts = {
+      debug = true,
+    },
   },
 })
